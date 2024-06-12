7bac478adf5fbaec54e71786eab6accb5b3b78eb
oDhruvPatel <dhruvpatel2721978@gmail.com>
2024-06-12 22:33:38 +0530

    Contact-from

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { IoMdContact } from "react-icons/io";
 
 interface FormData {
-  firstName: string;
-  category: string;
-  aboutYou: string;
+  name: string;
+  email: string;
+  description: string;
 }
 
 const ContactForm: React.FC = () => {
@@ -16,19 +17,21 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("firstName")} placeholder="First name" />
-        <select {...register("category", { required: true })}>
-          <option value="">Select...</option>
-          <option value="A">Option A</option>
-          <option value="B">Option B</option>
-        </select>
-        <textarea {...register("aboutYou")} placeholder="Write the Messege" />
-        <p>{data}</p>
-        <input type="submit" />
+    <>
+     <div className="md:w-[80%] w-[90%] p-5 border-2 shadow-md rounded-lg my-7 m-auto flex  flex-col bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200">
+     
+     <div className=" w-[100%] text-8xl text-indigo-600 flex flex-col justify-center items-center"> <IoMdContact/>
+     <div className="flex flex-row w-full"></div>
+      </div>
+     
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col primary text-xl w-[100%] text-slate-800">
+        <input {...register("name")} placeholder="Dhruv Patel" className="my-2 p-4 rounded-lg"/> 
+        <input {...register("email")} placeholder="example@gmail.com" className="my-2 p-4 rounded-lg"/>
+        <textarea {...register("email")} placeholder="Write your message here." className="my-2 p-4 rounded-lg"/>
+        <input type="submit" className="px-4 py-2  md:w-[30%] w-[50%] m-auto my-4 rounded-full focus:ring-4 focus:from-white focus:to-white focus:text-slate-800 text-white font-medium bg-gradient-to-r from-indigo-500 to-pink-500 hover:from-pink-500 hover:to-indigo-500  "/>
       </form>
     </div>
+    </>
   );
 };
 
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,8 +5,9 @@ const Experience = () => {
     const DA = "I am currently immersed in an enriching Data Analyst internship, where I'm actively refining my analytical prowess and gaining invaluable insights into the dynamic field of data science."
     return (
     <>
-      <h1 className='text-slate-800  text-3xl md:text-center text-center md:text-4xl primary capitalize font-bold  md:py-3 py-2 my-7  '>
+      <h1 className='text-slate-800 flex justify-center items-center text-3xl md:text-center text-center md:text-4xl primary capitalize font-bold  md:py-3 py-2 my-7  '>
              Experience
+             <svg className="h-full w-5 md:w-6 ml-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z"/></svg>
         </h1>
         
         <div
@@ -15,8 +16,10 @@ const Experience = () => {
         <div
           className={`view1 md:border-r-4 md:border-l-0 gradient-border border-l-4  border-slate-800 z-50 md:w-[50%] w-[90%] md:m-0 m-auto flex md:justify-center md:items-center flex-col `}
         >
-                  <Education seq={1} classLR="md:text-right md:pr-1" dotLR="md:-right-[10px] md:left-auto -left-[-3.5%]" class="left" title="Backend Developer (Node.js)" timeline="jan 2024 - Apr 2024" desc={Toshal}/>
-                  <Education seq={2} classLR="md:text-left md:pl-1"  dotLR="md:-left-[5px] md:right-auto -left-[-3.5%]" class="right" title="Data Analyst" timeline="May 2024 - Present" desc={DA}/>
+                  
+                  <Education seq={1} classLR="md:text-left md:pl-1"  dotLR="md:-left-[5px] md:right-auto -left-[-3.5%]" class="right" title="Data Analyst" timeline="May 2024 - Present" desc={DA}/>
+                  <Education seq={2} classLR="md:text-right md:pr-1" dotLR="md:-right-[10px] md:left-auto -left-[-3.5%]" class="left" title="Backend Developer (Node.js)" timeline="jan 2024 - Apr 2024" desc={Toshal}/>
+                  
         </div>
         </div>     
     </>
